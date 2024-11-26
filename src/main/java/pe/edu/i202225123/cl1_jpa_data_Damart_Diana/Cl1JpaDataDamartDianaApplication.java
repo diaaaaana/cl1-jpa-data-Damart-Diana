0afda293910df6b8bee37fce905565c3e1ad6fd4
diaaaaana <didaromaalan2005@gmail.com>
2024-11-25 20:39:32 -0500

    implementacion de CommandLineRunner en la clase principal

--- src/main/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/Cl1JpaDataDamartDianaApplication.java
+++ src/main/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/Cl1JpaDataDamartDianaApplication.java
@@ -1,13 +1,18 @@
 package pe.edu.i202225123.cl1_jpa_data_Damart_Diana;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cl1JpaDataDamartDianaApplication {
+public class Cl1JpaDataDamartDianaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cl1JpaDataDamartDianaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
