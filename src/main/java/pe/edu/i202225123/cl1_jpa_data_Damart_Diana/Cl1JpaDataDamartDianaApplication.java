3cc3e6e25dbd748a335439dd84dc276d5e543da6
diaaaaana <didaromaalan2005@gmail.com>
2024-11-25 19:58:24 -0500

    creacion proyecto

---
+++ src/main/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/Cl1JpaDataDamartDianaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202225123.cl1_jpa_data_Damart_Diana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JpaDataDamartDianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JpaDataDamartDianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/Cl1JpaDataDamartDianaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202225123.cl1_jpa_data_Damart_Diana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JpaDataDamartDianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
