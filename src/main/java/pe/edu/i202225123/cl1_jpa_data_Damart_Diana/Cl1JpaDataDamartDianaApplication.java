c3e054e82373f9b6d1851d847b34f90e05b42b7d
diaaaaana <didaromaalan2005@gmail.com>
2024-11-25 21:00:16 -0500

    funcionalidades de obtener pais argentina y eliminar colombia y argentina en la clase principal

--- src/main/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/Cl1JpaDataDamartDianaApplication.java
+++ src/main/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/Cl1JpaDataDamartDianaApplication.java
@@ -1,18 +1,48 @@
 package pe.edu.i202225123.cl1_jpa_data_Damart_Diana;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pe.edu.i202225123.cl1_jpa_data_Damart_Diana.model.Country;
+import pe.edu.i202225123.cl1_jpa_data_Damart_Diana.repository.CountryRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class Cl1JpaDataDamartDianaApplication implements CommandLineRunner {
 
+	@Autowired
+	CountryRepository countryRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(Cl1JpaDataDamartDianaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Optional<Country> argentina = countryRepository.findById("ARG");
+		argentina.ifPresentOrElse(
+				Country -> {
+					System.out.println("--------------------: Lenguajes de Argentina: ----------");
+					Country.getLanguages().forEach(language -> System.out.println(language.getLanguage()));
+					System.out.println("--------------------------------------------------");
+				},() -> {
+					Optional<Country> perCountry = countryRepository.findById("PER");
+					perCountry.ifPresent(per -> {
+						System.out.println("----------------:lengua de Perú :------------------");
+						per.getLanguages().forEach(lang -> System.out.println(lang.getLanguage()));
+						System.out.println("--------------------------------------------------");
+					});
+				}
+		);
+
+		// Lista de IDs de los países a eliminar
+		/*List<String> countryIdsToDelete = Arrays.asList("COL", "ARG");
+
+		countryRepository.deleteAllById(countryIdsToDelete);
+		System.out.println("Países eliminados con sus ciudades y lenguajes asociados");*/
 
 	}
 }
---
+++ src/main/java/pe/edu/i202225123/cl1_jpa_data_Damart_Diana/config/ConexionesConfig.java
@@ -0,0 +1,53 @@
+package pe.edu.i202225123.cl1_jpa_data_Damart_Diana.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConexionesConfig {
+
+    @Value("${DB_WORLD_URL}")
+    private String dbSakilaUrl;
+    @Value("${DB_ WORLD_USER}")
+    private String dbSakilaUser;
+    @Value("${DB_ WORLD_PASS}")
+    private String dbSakilaPass;
+    @Value("${DB_ WORLD_DRIVER}")
+    private String dbSakilaDriver;
+
+    @Bean
+    public HikariDataSource hikariDataSource() {
+
+        HikariConfig config = new HikariConfig();
+
+        /**
+         * Configurar propiedad de conexion a BD
+         */
+        config.setJdbcUrl(dbSakilaUrl);
+        config.setUsername(dbSakilaUser);
+        config.setPassword(dbSakilaPass);
+        config.setDriverClassName(dbSakilaDriver);
+
+        /**
+         * Configurar propiedades del pool de HikariCP
+         * - MaximumPoolSize: Máximo # de conexiones del pool.
+         * - MinimumIdle: Mínimo # de conexiones inactivas del pool.
+         * - IdleTimeout: Tiempo máximo de espera para
+         *      eliminar una conexión inactiva.
+         * - ConnectionTimeout: Tiempo máximo de espera
+         *      para conectarse a la BD.
+         */
+        config.setMaximumPoolSize(30); // cantidad de personas que se conectan al mismo tiempo
+        config.setMinimumIdle(4); //inactivos
+        config.setIdleTimeout(240000); // 4 minutos en milisegundosw
+        config.setConnectionTimeout(45000); //tiempo de conexion 45 segundos
+
+        System.out.println("###### HikariCP initialized ######");
+        return new HikariDataSource(config);
+
+    }
+
+}
